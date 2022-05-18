637fd3c9a398b4f921bed8cf39f11fb679166a6d
oao123 <87715417+oao123@users.noreply.github.com>
2022-05-18 16:02:40 -0300

    Create MinhaThread2.java

---
+++ MinhaThread2.java
@@ -0,0 +1,23 @@
+public class MinhaThread2 implements Runnable{
+    double pi = 0,soma2=0;
+  //int u=0,t=1;
+    double[] soma = new double[20];
+  public MinhaThread2(double[] soma ){
+   this.soma=soma;
+ }
+    public void run(){
+        
+        for(int i =30;i<50;i++){
+              pi = ((Math.pow(-1, i))/((2*i)+1));
+
+          soma[1]=pi+soma[1];
+          //soma=pi+soma2;
+          //soma2=soma;
+         // System.out.println(soma[1]);
+        }
+     // soma[0]=3;
+       pi+=4;
+    
+    // System.out.println(soma);
+  }
+}
